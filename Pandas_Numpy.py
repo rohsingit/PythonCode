2578d597ffa2bb3a1461836796b3830c3a2a2b4f
Rohit Singh <rosingh@flowserve.com>
2018-04-10 16:46:15 +0800

    Changes to  pandas

--- Pandas_Numpy.py
+++ Pandas_Numpy.py
@@ -3,15 +3,26 @@ import matplotlib.pyplot as plt
 
 ##READ FILES/GENERATE DATA
 #From CSV
-path = r"C:\Users\10154861\Desktop\Work\1. Analytics\A. Data\A. VM_Holz_Biomasse\C. 4_yrs_complete\2985.csv"
+path = r"C:\Users\10154861\Desktop\Work\1. Analytics\A. Data\A. VM_Holz_Biomasse\C. 4_yrs_complete\2990.csv"
 df = pd.read_csv(path)
 df.isnull().sum()
 
-path = r"C:\Users\10154861\Desktop\Work\1. Analytics\A. Data\A. VM_Holz_Biomasse\A. 4_yrsData_IPS_Detect_iCMS\D1_P003_SIHI_MSC_065140_non_drive_side_2985_7.csv"
-df2 = pd.read_csv(path)
 
-path = r"C:\Users\10154861\Desktop\Work\1. Analytics\A. Data\A. VM_Holz_Biomasse\A. 4_yrsData_IPS_Detect_iCMS\D1_P003_SIHI_MSC_065140_drive_side_2990_6.csv"
-df3 = pd.read_csv(path)
+# path = r"C:\Users\10154861\Desktop\Work\1. Analytics\A. Data\A. VM_Holz_Biomasse\A. 4_yrsData_IPS_Detect_iCMS\D1_P003_SIHI_MSC_065140_non_drive_side_2985_7.csv"
+# df2 = pd.read_csv(path)
+#
+# path = r"C:\Users\10154861\Desktop\Work\1. Analytics\A. Data\A. VM_Holz_Biomasse\A. 4_yrsData_IPS_Detect_iCMS\D1_P003_SIHI_MSC_065140_drive_side_2990_6.csv"
+# df3 = pd.read_csv(path)
+
+##READ FILES FROM FOLDERS
+# import glob, os
+#
+# path = r"C:\Users\10154861\Desktop"
+# fileNames = glob.glob(path + "*.tdms")
+#
+# for f in fileNames:
+#     file = os.path.basename(f)
+#     print("Reading: " +file)
 
 #Create DataFrame
 # df = {'Filename': [], 'Pump_Model': [], 'Serial_Number': [], 'Motor_Number': []}
@@ -25,8 +36,14 @@ df3 = pd.read_csv(path)
 # print(df.index)
 
 ##Manipulate Columns
-list(df)            #List column names
+# list(df)            #List column names
+# df = df.rename(columns={'oldName1': 'newName1', 'oldName2': 'newName2'})
 
+
+##Convert time from UNIX to usual format
+df['Time'] = pd.to_datetime(df['Timestamp'], unit = 's')
+df.set_index('Time', inplace= True)
+del df['Timestamp']
 
 ##Check for missing cells
 # df.isnull().sum()
@@ -44,7 +61,9 @@ list(df)            #List column names
 # #
 # # plt.show()
 
-
+# plt.plot(df)
+df.corr()
+pd.plotting.scatter_matrix(df)
 
 
 # X['workClass'] = X['workClass'].astype('category')
